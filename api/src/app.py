0c0eb8387e63d77626209a8b3a90455c4fa6b55e
enes <ef1816@student.uni-lj.si>
2024-04-04 12:55:32 +0200

    Updated routes and added some db functions

--- api/src/app.py
+++ api/src/app.py
@@ -1,27 +1,34 @@
 import db_operations as dbo
-import bcrypt
+# import bcrypt
 from flask import Flask, jsonify
 from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 basic_auth = HTTPBasicAuth()
 
+
 @basic_auth.verify_password
 def verify_password(username, password):
     # Simple development authentication placeholder
     return username == 'admin' and password == 'admin'
 
+
 @basic_auth.error_handler
 def basic_auth_error(status):
     return jsonify({"success": False, "message": "Wrong credentials!"}), status
 
+
 @app.errorhandler(404)
 def not_found_error(error):
-    return jsonify({"success": False, "message": "Object not found!", "error": error}), 404
+    return jsonify({"success": False, "message": "Object not found!",
+                    "error": error}), 404
+
 
 @app.errorhandler(500)
 def internal_error(error):
-    return jsonify({"success": False, "message": "Server error!", "error": error}), 500
+    return jsonify({"success": False, "message": "Server error!",
+                    "error": error}), 500
+
 
 @app.route("/", methods=['GET'])
 def home():
@@ -30,11 +37,13 @@ def home():
         print(html_content)
     return html_content
 
-@app.route("/test_connection" , methods=['GET'])
+
+@app.route("/test_connection", methods=['GET'])
 @basic_auth.login_required
 def test_connection():
     retVal = dbo.test_db_connection()
     return jsonify(retVal)
+
 
 @app.route("/execute_script/<string:script_name>", methods=['POST'])
 @basic_auth.login_required
@@ -42,18 +51,78 @@ def execute_script(script_name):
     retVal = dbo.execute_sql_script(script_name)
     return jsonify(retVal)
 
+
 @app.route("/drop_db", methods=['POST'])
 @basic_auth.login_required
 def drop_db():
     # retVal = dbo.drop_database()
     # return jsonify(retVal)
-    return jsonify({"success": False, "error": "This operation is disabled! Enable it by commenting out the code."})
+    return jsonify({"success": False, "error": "This operation is disabled!"
+                    " Enable it by commenting out the code."})
+
 
 @app.route("/get_tables", methods=['GET'])
 @basic_auth.login_required
 def get_tables():
     tables = dbo.get_all_tables()
     return jsonify(tables)
+
+
+@app.route("/get_frontier_pages", methods=['GET'])
+@basic_auth.login_required
+def get_frontier_pages():
+    cursor = dbo.get_frontier_pages()
+    return jsonify(cursor)
+
+
+@app.route("/insert_site", methods=['POST'])
+@basic_auth.login_required
+def insert_site():
+    site_id = dbo.insert_site()
+    return jsonify("Site ID:", site_id)
+
+
+@app.route("/insert_image", methods=['POST'])
+@basic_auth.login_required
+def insert_image():
+    retVal = dbo.insert_image()
+    return jsonify(retVal)
+
+
+@app.route("/insert_page_into_frontier", methods=['POST'])
+@basic_auth.login_required
+def insert_page_into_frontier():
+    retVal = dbo.insert_page_into_frontier()
+    return jsonify(retVal)
+
+
+@app.route("/get_frontier_length", methods=['GET'])
+@basic_auth.login_required
+def get_frontier_length():
+    length = dbo.get_frontier_length()
+    return jsonify("Frontier length:", length)
+
+
+@app.route("/update_page_data", methods=['POST'])
+@basic_auth.login_required
+def update_page_data():
+    retVal = dbo.update_page_data()
+    return jsonify(retVal)
+
+
+@app.route("/get_page", methods=['GET'])
+@basic_auth.login_required
+def get_page():
+    cursor = dbo.get_page()
+    return jsonify(cursor)
+
+
+@app.route("/get_site", methods=['GET'])
+@basic_auth.login_required
+def get_site():
+    cursor = dbo.get_site()
+    return jsonify(cursor)
+
 
 if __name__ == '__main__':
     app.run(ssl_context='adhoc')
--- api/src/db_operations.py
+++ api/src/db_operations.py
@@ -4,6 +4,7 @@ import threading
 
 lock = threading.Lock()
 
+
 def get_database_connection():
     host = os.environ.get("CLOUD_SQL_HOST")
     port = int(os.environ.get("CLOUD_SQL_PORT"))
@@ -25,13 +26,16 @@ def get_database_connection():
         print(f"Error connecting to database: {e}")
         raise e
 
+
 def test_db_connection():
     try:
         conn = get_database_connection()
         conn.close()
-        return {"success": True, "message": "Connection to database successful!"}
+        return {"success": True, "message":
+                "Connection to database successful!"}
     except psycopg2.Error as e:
         return {"success": False, "error": str(e)}
+
 
 def execute_sql_script(script_file_path):
     try:
@@ -44,31 +48,219 @@ def execute_sql_script(script_file_path):
                 conn.commit()
                 cur.close()
                 conn.close()
-                return {"success": True, "message": f"Script '{script_file_path}' executed successfully!"}
+                return {"success": True, "message":
+                        f"Script '{script_file_path}' executed successfully!"}
         else:
-            return {"success": False, "error": f"Script '{script_file_path}' does not exist."}
+            return {"success": False, "error":
+                    f"Script '{script_file_path}' does not exist."}
     except Exception as e:
         return {"success": False, "error": str(e)}
+
 
 def drop_database():
     try:
         conn = get_database_connection()
         conn.autocommit = True
         cur = conn.cursor()
-        cur.execute(f"DROP DATABASE IF EXISTS {os.environ.get('CLOUD_SQL_DATABASE')}")
+        cur.execute(f"DROP DATABASE IF EXISTS"
+                    f" {os.environ.get('CLOUD_SQL_DATABASE')}")
         cur.close()
         conn.close()
-        return {"success": True, "message": f"Database '{os.environ.get('CLOUD_SQL_DATABASE')}' dropped successfully!"}
+        return {"success": True, "message": f"Database"
+                f" '{os.environ.get('CLOUD_SQL_DATABASE')}'"
+                f" dropped successfully!"}
     except Exception as e:
         return {"success": False, "error": str(e)}
+
 
 def get_all_tables():
     try:
         conn = get_database_connection()
         cur = conn.cursor()
-        cur.execute(f"SELECT table_name FROM information_schema.tables WHERE table_schema = '{os.environ.get('CLOUD_SQL_DATABASE')}'")
+        cur.execute(f"SELECT table_name FROM information_schema.tables"
+                    f" WHERE table_schema ="
+                    f" '{os.environ.get('CLOUD_SQL_DATABASE')}'")
         tables = cur.fetchall()
         all_tables = [table[0] for table in tables]
-        return {"success": True, "message": f"Successfully retrieved all tables from '{os.environ.get('CLOUD_SQL_DATABASE')}'.", "data": all_tables}
+        return {"success": True,
+                "message": f"Successfully retrieved all"
+                f" tables from '{os.environ.get('CLOUD_SQL_DATABASE')}'.",
+                "data": all_tables}
     except Exception as e:
         return {"success": False, "error": str(e)}
+
+
+def get_frontier_pages(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT id, url FROM crawldb.page "
+            "WHERE page_type_code = 'FRONTIER' "
+            "ORDER BY id"
+        )
+        conn.commit()
+        return cursor.fetchall()
+    except Exception as e:
+        print("Error while fetching frontier pages:", e)
+        return []
+
+
+def insert_site(conn, domain, robots_content, sitemap_content):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO crawldb.site "
+            "(domain, robots_content, sitemap_content) "
+            "VALUES (%s, %s, %s) RETURNING id",
+            (domain, robots_content, sitemap_content)
+        )
+        site_id = cursor.fetchone()[0]
+        conn.commit()
+        print("Site data inserted successfully with ID:", site_id)
+        return site_id
+    except Exception as e:
+        print("Error while inserting site data:", e)
+        conn.rollback()
+        return None
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+
+def insert_image(conn, url, filename, content_type, accessed_time):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT id FROM crawldb.page WHERE url = %s", (url,)
+        )
+        page_id = cursor.fetchone()[0]
+        cursor.execute(
+            "INSERT INTO crawldb.image "
+            "(page_id, filename, content_type, accessed_time) "
+            "VALUES (%s, %s, %s, %s)",
+            (page_id, filename, content_type, accessed_time)
+        )
+        conn.commit()
+        print("Image data inserted successfully.")
+    except Exception as e:
+        print("Error while inserting image data:", e)
+        conn.rollback()
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+
+def insert_page_into_frontier(conn, domain, url, page_type='FRONTIER',
+                              from_page=None):
+    try:
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT id FROM crawldb.site"
+                       " WHERE domain = %s", (domain,))
+        site_id = cursor.fetchone()
+        if site_id is None:
+            # Create a new site entry
+            # site_id = insert_site(conn, domain, None, None, None)
+            pass
+        # else:
+            # site_id = site_id[0]
+
+        print('site_id', site_id)
+        cursor.execute(
+            "INSERT INTO crawldb.page (site_id, url, page_type_code) "
+            "VALUES (%s, %s, %s) RETURNING id",
+            (0, url, page_type)
+        )
+        to_page_id_result = cursor.fetchone()
+
+        if from_page is not None and to_page_id_result is not None:
+            to_page_id = to_page_id_result[0]
+            cursor.execute("SELECT id FROM crawldb.page "
+                           "WHERE url = %s", (from_page,))
+            from_page_id_result = cursor.fetchone()
+            if from_page_id_result is not None:
+                from_page_id = from_page_id_result[0]
+                cursor.execute(
+                    "INSERT INTO crawldb.crawl_links (from_page, to_page) ",
+                    "VALUES (%s, %s)",
+                    (from_page_id, to_page_id)
+                )
+        conn.commit()
+        print("Page inserted into frontier successfully.")
+    except Exception as e:
+        print("Error while inserting page into frontier:", e)
+        conn.rollback()
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+
+def get_frontier_length(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT COUNT(*) FROM crawldb.page ",
+            "WHERE page_type_code = 'FRONTIER'"
+        )
+        length = cursor.fetchone()[0]
+        print("Frontier length:", length)
+        return length
+    except Exception as e:
+        print("Error while fetching frontier length:", e)
+        return 0
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+
+def update_page_data(conn, url, page_type):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            "UPDATE crawldb.page SET page_type_code = %s ",
+            "WHERE url = %s",
+            (page_type, url)
+        )
+        conn.commit()
+        print("Page data updated successfully.")
+    except Exception as e:
+        print("Error while updating page data:", e)
+        conn.rollback()
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+
+def get_page(conn, url):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT id, site_id, url, page_type_code ",
+            "FROM crawldb.page WHERE url = %s",
+            (url,)
+        )
+        return cursor.fetchone()
+    except Exception as e:
+        print("Error while fetching page data:", e)
+        return None
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+
+def get_site(conn, domain):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT id, domain, robots_content, sitemap_content, ip_address ",
+            "FROM crawldb.site WHERE domain = %s",
+            (domain,)
+        )
+        return cursor.fetchone()
+    except Exception as e:
+        print("Error while fetching site data:", e)
+        return None
+    finally:
+        if cursor is not None:
+            cursor.close()
+
